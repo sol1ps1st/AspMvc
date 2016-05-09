bf18112383d6443ff3d77f231e3e37912d12b13e
Alex <alvi88@list.ru>
2016-05-09 17:04:58 +0300

    4 модуль. Созданы классы моделей - Book и Purchase. Определен контекст данных, содержащий совйства-множества книг и покупок. Создан класс - заполнитель БД.

---
+++ BookingAppStore/BookingAppStore/Models/BookContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BookingAppStore.Models
+{
+    public class BookContext: DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+
+    public class BookDbInitializer: DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            context.Books.Add(new Book() { Name = "Война и мир", Author = "Л. Толстой", Price = 220 });
+            context.Books.Add(new Book() { Name = "Отцы и дети", Author = "И. Тургенев", Price = 180 });
+            context.Books.Add(new Book() { Name = "Чайка", Author = "А. Чехов", Price = 150 });
+            base.Seed(context);
+        }
+    }
+}
